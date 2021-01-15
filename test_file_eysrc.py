15da66a87c1d98250f0e190a63358bb1856cb9e4
siddhesh-sinha <53999351+siddhesh-sinha@users.noreply.github.com>
2021-01-15 20:13:05 +0530

    test push

--- test_file_eysrc.py
+++ test_file_eysrc.py
@@ -1,10 +1,11 @@
-
-import cv2
+import cv2 as cv
 
 # read image as grey scale
-grey_img = cv2.imread('/path/to/img', cv2.IMREAD_GRAYSCALE)
-
-# save image
-save = cv2.imwrite('/path/to/img.png', grey_img)
-
+img = cv.imread('E:\computer\python\lol\oh damn son\Resources\Photos/baground.jpg')
+img = cv.resize(img,(1280,720))
+grey_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+HSV_img = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+save = cv.imwrite('E:\computer\python\lol\oh damn son\Resources\Baground_greyscale.jpg', grey_img)
+print("Image written to file-system : ", save)
+save = cv.imwrite('E:\computer\python\lol\oh damn son\Resources\Baground_HSV.jpg', HSV_img)
 print("Image written to file-system : ", save)
